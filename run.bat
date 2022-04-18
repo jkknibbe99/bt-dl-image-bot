c3b81a66d419fe7076c14655eaff2c95240accd3
Josh <jkknibbe99@gmail.com>
2022-04-18 10:21:31 -0400

    run.bat updated

--- run.bat
+++ run.bat
@@ -1,12 +1,35 @@
-@ECHO OFF
+@REM @ECHO OFF
 
+:: Run python config file
+python config.py
+
+:: Make it possible to read immediate value of variable using !variable! syntax.
+setlocal enabledelayedexpansion
+
+:: Read file "init.json" into variable data
+set data=
+for /f "delims=" %%x in (init.json) do set "data=!data!%%x"
+rem Remove quotes
+set data=%data:"=%
+rem Remove braces
+set "data=%data:~2,-1%"
+rem Change colon+space by equal-sign
+set "data=%data:: ==%"
+rem Separate parts at comma into individual assignments
+set "%data:, =" & set "%"
+
+rem check if a venv exists
 if exist Scripts (
-    ECHO Scripts Dir exists
+    echo Scripts Dir exists
+    call Scripts\activate
+    rem run bot
+    python %bot_path%
 ) else (
-    ECHO Scripts Dir could not be found
+    ECHO Virtual environment not created.
+    ECHO Creating Virtual environment now...
+    python -m venv .
+    Scripts\activate
+    pip install -r requirements.txt
+    rem run bot
+    python %bot_path%
 )
-
-PAUSE
-
-:: Scripts\activate
-
