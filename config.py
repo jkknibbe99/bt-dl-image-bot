68fe9e5be03000aff692b028c5e70d6c7748f6a4
Josh <jkknibbe99@gmail.com>
2022-04-18 10:21:11 -0400

    Added more config data

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import json
+
+# Data
+init_data = {
+    'bot_path': 'bot.py' 
+}
+
 login_data = {
     'login_url': 'https://buildertrend.net/',
     'username': 'joshua@engelsmahomes.com',
@@ -5,5 +12,15 @@ login_data = {
 }
 
 chromedriver_data = {
-    'version': 100
+    'version': 100,
+    'directory': '/'
 }
+
+# Main
+if __name__ == '__main__':
+    # Serializing json 
+    json_object = json.dumps(init_data, indent = 4)
+  
+    # Writing to init.json
+    with open("init.json", "w") as outfile:
+        outfile.write(json_object)
