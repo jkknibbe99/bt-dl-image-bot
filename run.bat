e3de16386ecb8a5a91550c685733faf103bb78db
Josh <jkknibbe99@gmail.com>
2022-04-18 11:11:05 -0400

    run scripts updated

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-@REM @ECHO OFF
+@ECHO OFF
 
 :: Run python config file
 python config.py
@@ -13,23 +13,28 @@ rem Remove quotes
 set data=%data:"=%
 rem Remove braces
 set "data=%data:~2,-1%"
-rem Change colon+space by equal-sign
+rem Change colon+space to equal-sign
 set "data=%data:: ==%"
 rem Separate parts at comma into individual assignments
 set "%data:, =" & set "%"
 
+cls
+
 rem check if a venv exists
 if exist Scripts (
-    echo Scripts Dir exists
+    echo Scripts directory exists
+    echo Running bot
     call Scripts\activate
     rem run bot
+    set bot_path=%bot_path:/=\%
     python %bot_path%
 ) else (
-    ECHO Virtual environment not created.
-    ECHO Creating Virtual environment now...
+    echo Virtual environment not created.
+    echo Creating Virtual environment now...
     python -m venv .
     call Scripts\activate
     pip install -r requirements.txt
     rem run bot
+    set bot_path=%bot_path:/=\%
     python %bot_path%
 )
--- run.sh
+++ run.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
-# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# Only run script if on a Mac
+if [[ $OSTYPE == 'darwin'* ]]; then
+  # cd to this script's dir
+  SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
+  cd $SCRIPT_DIR
 
-cd $SCRIPT_DIR
+  # check if venv present
+  BIN_DIR="$SCRIPT_DIR/bin/"
+  if [ -d "$BIN_DIR" ]; then
+    ### virtual env bin dir was found ###
+    echo "Venv found."
+    source bin/activate
+  else
+    ### create virtual environment if one does not already exist ###
+    echo "No venv found. Creating one now."
+    virtualenv --python=python3 .
+    source bin/activate
+    pip install -r requirements.txt
+  fi
 
-BIN_DIR="$SCRIPT_DIR/bin/"
-if [ -d "$BIN_DIR" ]; then
-  ### virtual env bin dir was found ###
-  echo "Venv found."
-  source bin/activate
+  # make chrome driver executable
+  chmod +x chromedriver_100_mac
+
+  # run bot
+  python config.py
+  BOT_PATH="$(grep -o '"bot_path": "[^"]*' init.json | grep -o '[^"]*$')"
+  python $BOT_PATH
 else
-  ### create virtual environment if one does not already exist ###
-  echo "No venv found. Creating one now."
-  virtualenv --python=python3 .
-  source bin/activate
-  pip install -r requirements.txt
+  echo Error: This script can only be run on a MacOS
+  read -p "Press [ENTER] to close ..."
 fi
-
-# make chrome driver executable
-chmod +x chromedriver_100_mac
-
-# run bot
-python bot.py 
