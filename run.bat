127589877943c6e9022b7c58a1427d6f55e068df
Josh <jkknibbe99@gmail.com>
2022-04-20 17:27:48 -0400

    added all absolute paths

--- run.bat
+++ run.bat
@@ -1,14 +1,15 @@
 @ECHO OFF
 
+
 :: Run python config file
-python config.py
+python %~dp0bot\config.py
 
 :: Make it possible to read immediate value of variable using !variable! syntax.
 setlocal enabledelayedexpansion
 
 :: Read file "init.json" into variable data
 set data=
-for /f "delims=" %%x in (init.json) do set "data=!data!%%x"
+for /f "delims=" %%x in (%~dp0data/init.json) do set "data=!data!%%x"
 rem Remove quotes
 set data=%data:"=%
 rem Remove braces
@@ -20,21 +21,22 @@ set "%data:, =" & set "%"
 
 cls
 
+rem Set bot_path
+set bot_path=%~dp0%bot_path:/=\%
+
 rem check if a venv exists
 if exist Scripts (
     echo Scripts directory exists
     echo Running bot
-    call Scripts\activate
+    call %~dp0Scripts\activate
     rem run bot
-    set bot_path=%bot_path:/=\%
     python %bot_path%
 ) else (
     echo Virtual environment not created.
     echo Creating Virtual environment now...
     python -m venv .
-    call Scripts\activate
+    call %~dp0Scripts\activate
     pip install -r requirements.txt
     rem run bot
-    set bot_path=%bot_path:/=\%
     python %bot_path%
 )
