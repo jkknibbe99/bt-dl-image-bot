8d0bf54f4ceb1265cc81bf019bf0a80b49ad3ad5
Josh <jkknibbe99@gmail.com>
2022-04-28 10:41:01 -0400

    changed path to Program Files (x86)

--- setup/download_python.ps1
+++ setup/download_python.ps1
@@ -11,11 +11,21 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 # Python install variables
 $pythonVersion = "3.10.4"
 $pythonUrl = "https://www.python.org/ftp/python/$pythonVersion/python-$pythonVersion.exe"
-$pythonDownloadPath = "C:\Program Files\python-$pythonVersion-installer.exe"
-$pythonInstallDir = "C:\Program Files\Python$pythonVersion"
+$pythonDownloadPath = "C:\Program Files (x86)\Python\python-$pythonVersion-installer.exe"
+$pythonInstallDir = "C:\Program Files (x86)\Python\Python$pythonVersion"
+
+# Create the Python/ directory if needed
+if (Test-Path -Path 'C:\Program Files (x86)\Python') {
+    # Path exists
+} else {
+    # Create Python dir
+    New-Item -Path 'C:\Program Files (x86)\Python' -ItemType Directory
+}
 
 # Run installer
 (New-Object Net.WebClient).DownloadFile($pythonUrl, $pythonDownloadPath)
 & $pythonDownloadPath InstallAllUsers=1 PrependPath=0 Include_test=0 TargetDir=$pythonInstallDir
 # Append to PATH environment variable
 [Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";" + $pythonInstallDir, [EnvironmentVariableTarget]::Machine)
+
+exit $LASTEXITCODE
